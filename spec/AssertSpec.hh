66951c849e832a34d2f85820942b57a7a753e43d
holyshared <holy.shared.design@gmail.com>
2015-06-24 19:46:00 +0900

    add spec

---
+++ spec/AssertSpec.hh
@@ -0,0 +1,18 @@
+<?hh //partial
+
+use hhassert\Assert;
+
+describe(Assert::class, function() {
+    describe('ok', function() {
+        context('when true', function() {
+            it('does nothing', function() {
+            });
+        });
+    });
+    describe('equal', function() {
+        context('when same value', function() {
+            it('does nothing', function() {
+            });
+        });
+    });
+});
--- src/Assert.hh
+++ src/Assert.hh
@@ -5,8 +5,16 @@ namespace hhassert;
 class Assert
 {
 
+    public static function ok<Ta>(Ta $value) : void
+    {
+        if ($value === true) {
+        }
+    }
+
     public static function equal<Ta, Te>(Ta $actual, Te $expected) : void
     {
+        if ($actual === $expected) {
+        }
     }
 
 }
